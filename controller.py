c21c903ec38b161ebe287793f2382572e27660fa
yash <ovando.yashua@gmailc.om>
2020-04-26 21:06:23 -0700

    removed images-these images didn;t work

--- controller.py
+++ controller.py
@@ -10,6 +10,6 @@ evaluation = eval.Eval()
 #     similar_pics = imageFinder.getImagesForGroup(group_id)
 #     eval.avg_precision_recal(similar_pics , group_id)
 
-group_id = 5
+group_id = 1
 similar_pics = imageFinder.getImagesForGroup(group_id)
 print(similar_pics)
